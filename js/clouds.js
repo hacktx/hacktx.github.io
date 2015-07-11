be38c238d97cda9906e6eeec88d984ce15bd9f57
nlaz <nikolazaris@utexas.edu>
2015-07-11 16:27:48 -0400

    clouds with animation

--- js/clouds.js
+++ js/clouds.js
@@ -1,11 +1,42 @@
 $( document ).ready(function() {
+
   animate({
-    el: '.cloud1',
+    el: '#cloud1',
     translateX: '100vw',
-    duration: 90000,
+    duration: 100000,
     easing: 'linear',
     complete: function() { 
-      $('.cloud1').remove();
+      $('#cloud1').remove();
+    }
+  });
+
+  animate({
+    el: '#cloud2',
+    translateX: '-100vw',
+    duration: 150000,
+    easing: 'linear',
+    complete: function() { 
+      $('#cloud2').remove();
+    }
+  });
+
+  animate({
+    el: '#cloud3',
+    translateX: '-100vw',
+    duration: 300000,
+    easing: 'linear',
+    complete: function() { 
+      $('#cloud3').remove();
+    }
+  });
+
+  animate({
+    el: '#cloud4',
+    translateX: '100vw',
+    duration: 500000,
+    easing: 'linear',
+    complete: function() { 
+      $('#cloud4').remove();
     }
   });
 });
