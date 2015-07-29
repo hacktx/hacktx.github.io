e360342ec7ab2c0bfaa568e6de06a11f2ecb4d17
drewromanyk <drewiswaycool@gmail.com>
2015-07-29 01:14:06 -0500

    added typed for HackTx About Screen

---
+++ js/HackTxTyped.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+
+  $(".HackTxTyped").typed({
+    strings: ['HACKING', 'DESIGNING', 'BUILDING', 'CREATING', 'MAKING', 'YOU'],
+    typeSpeed: 100,
+    loop: true
+  });
+
+});
