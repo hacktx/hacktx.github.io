cad9794cc2e30b3d2990688f08b223390a6f5961
nlaz <nikolazaris@utexas.edu>
2015-07-30 01:21:23 -0400

    updated typed title

--- js/HackTxTyped.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-
-  $(".HackTxTyped").typed({
-    strings: ['HACKING', 'DESIGNING', 'BUILDING', 'CREATING', 'MAKING', 'YOU'],
-    typeSpeed: 175,
-    loop: true
-  });
-
-});
