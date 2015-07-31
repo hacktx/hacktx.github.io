755c22c6249eb01d020cb5d1969835b4c237ce06
drewromanyk <drewiswaycool@gmail.com>
2015-07-30 21:46:27 -0500

    added fading for skyline

---
+++ js/_main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Drew on 7/30/15.
+ */
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
