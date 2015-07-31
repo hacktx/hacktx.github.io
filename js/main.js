ea630b19287be4c5a65bda1d82a9536482b5f79a
drewromanyk <drewiswaycool@gmail.com>
2015-07-31 01:11:21 -0500

    fixing skroller effects

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 /**
  * Created by Drew on 7/30/15.
  */
-( function( $ ) {
-    // Init Skrollr
+$( document ).ready(function() {
     var s = skrollr.init({
         render: function(data) {
             //Debugging - Log the current scroll position.
             //console.log(data.curTop);
         }
     });
-} )( jQuery );
+});
