ea2c85ca28c193936be89e80d735af32c4a10e64
drewromanyk <drewiswaycool@gmail.com>
2015-07-31 14:48:22 -0500

    changed movement for clouds

--- js/clouds.js
+++ js/clouds.js
@@ -1,19 +1,31 @@
 $( document ).ready(function() {
 
-  animateCloud1_1();
+  var vH=$('body').height();
+  var vW=$('body').width();
 
-  animateCloud2_1();
+  var skyContainer = $("#skyContainer");
+  var cloud1 = $("#cloud1");
+  var cloud2 = $("#cloud2");
+  var cloud3 = $("#cloud3");
+  var cloud4 = $("#cloud4");
 
-  animateCloud3_1();
-  animateCloud4_1();
+  skyContainer.mousemove(function(event) {
+    var newXOffset = event.pageX / 500;
+    var newYOffset = event.pageY / 500;
+    cloud1.css({"left" : 4 - newXOffset + "%", "top" : 20 - newYOffset + "%"});
+    cloud2.css({"right" : 10 + newXOffset + "%", "top" : 20 - newYOffset + "%"});
+    cloud3.css({"right" : 25 + newXOffset + "%", "top" : 10 - newYOffset + "%"});
+    cloud4.css({"left" : 21 - newXOffset + "%", "top" : 5 - newYOffset + "%"});
+  });
 });
+
 
 function animateCloud1_1() {
   animate({
     //100000
     el: '#cloud1',
     translateX: '5vw',
-    duration: 10000,
+    duration: 100000,
     easing: 'linear',
     complete: function() {
       animateCloud1_2();
@@ -26,7 +38,7 @@ function animateCloud1_2() {
     //100000
     el: '#cloud1',
     translateX: '-5vw',
-    duration: 10000,
+    duration: 100000,
     easing: 'linear',
     complete: function() {
       animateCloud1_1();
@@ -39,7 +51,7 @@ function animateCloud2_1() {
     //100000
     el: '#cloud2',
     translateX: '-5vw',
-    duration: 15000,
+    duration: 150000,
     easing: 'linear',
     complete: function() {
       animateCloud2_2();
@@ -52,7 +64,7 @@ function animateCloud2_2() {
     //100000
     el: '#cloud2',
     translateX: '5vw',
-    duration: 15000,
+    duration: 150000,
     easing: 'linear',
     complete: function() {
       animateCloud2_1();
@@ -65,7 +77,7 @@ function animateCloud3_1() {
     //100000
     el: '#cloud3',
     translateX: '-10vw',
-    duration: 30000,
+    duration: 300000,
     easing: 'linear',
     complete: function() {
       animateCloud3_2();
@@ -78,7 +90,7 @@ function animateCloud3_2() {
     //100000
     el: '#cloud3',
     translateX: '10vw',
-    duration: 30000,
+    duration: 300000,
     easing: 'linear',
     complete: function() {
       animateCloud3_1();
