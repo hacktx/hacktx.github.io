e7f4f51a782db29def430f2eeeef0fa59be69922
drewromanyk <drewiswaycool@gmail.com>
2015-08-02 00:23:25 -0500

    workin on clouds and skyline

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
  * Created by Drew on 7/30/15.
  */
 $( document ).ready(function() {
+
     var s = skrollr.init({
         render: function(data) {
             //Debugging - Log the current scroll position.
