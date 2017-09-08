8030eeb1298570f5833c55aeee999daa8d644f91
Raymond Chee <rjchee@gmail.com>
2017-09-08 02:49:10 -0500

    Add google form mailing list button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 $page = $(".site-wrapper")
 
-sponsorship = function() {
-  window.location = "mailto:partnerships@freetailhackers.com"
-}
-
-$page.on('click', '.sponsorship', sponsorship);
+$page.on('click', '#sponsorship', function() { window.location = "mailto:partnerships@freetailhackers.com"; });
+$page.on('click', '#interest-form', function() { window.open("https://goo.gl/forms/nyrwpi4dSA9vESwI3", "_blank"); });
