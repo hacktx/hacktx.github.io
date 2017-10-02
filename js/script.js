bf9048f0404427ee2b7d626e8b8811fd6a293fb4
Shreyas Sood <shreyassood@users.noreply.github.com>
2017-10-02 17:04:50 -0500

    Added registration button for hacktx 2017 (#27)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 $page = $(".site-wrapper")
 
 $page.on('click', '#sponsorship', function() { window.location = "mailto:partnerships@freetailhackers.com"; });
-$page.on('click', '#interest-form', function() { window.open("https://goo.gl/forms/nyrwpi4dSA9vESwI3", "_blank"); });
+$page.on('click', '#register-link', function() { window.location = "https://my.hacktx.com/"; });
