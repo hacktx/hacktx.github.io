d64e287db1317bbd13c0ff652e15162c37b4133b
Shreyas Sood <shreyassood@gmail.com>
2018-09-07 14:31:44 -0500

    Change scrolling method

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+	function smoothScroll(el, to, duration) {
+	    if (duration < 0) {
+	        return;
+	    }
+	    var difference = to - $(window).scrollTop();
+	    var perTick = difference / duration * 10;
+	    this.scrollToTimerCache = setTimeout(function() {
+	        if (!isNaN(parseInt(perTick, 10))) {
+	            window.scrollTo(0, $(window).scrollTop() + perTick);
+	            smoothScroll(el, to, duration - 10);
+	        }
+	    }.bind(this), 10);
+	}
 	$("#what object").on("load", function() {
 		var svg = this.contentDocument,
 			els = svg.querySelectorAll("#square1, #triangle1, #circle1"),
@@ -167,7 +180,7 @@ $(document).ready(function() {
 
 	$("header .cta").click(function(e) {
 		e.preventDefault();
-		$(window).scrollToTop([$("#register").offset().top]);
+		smoothScroll($(window), $("#register").offset().top, 400);
 	});
 
 	// Countdown
