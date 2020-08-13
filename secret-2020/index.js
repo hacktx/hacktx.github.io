ee2cb8955893181aa086b1b4f904351437cef26e
Arjun Kunjilwar <arjunkunjilwar@utexas.edu>
2020-08-12 21:12:42 -0500

    Center hacktx logo if screen size small

--- secret-2020/index.js
+++ secret-2020/index.js
@@ -14,25 +14,41 @@ function imageHoverOut(element) {
     element.setAttribute('src', newSrc)
 }
 
-var pos=0;
-$(window).bind('mousewheel DOMMouseScroll', function(event){
-    console.log("HI")
-    if (event.originalEvent.wheelDelta <= 0 && event.originalEvent.detail >= 0) {              
-        pos=pos+50;         
-    }
-    else {
-        if (pos > 1){
-            pos=pos-50;      
-        }
-    }
-    $('#superContentContainer').scrollLeft( pos)
-});
+// var pos=0;
+// $(window).bind('mousewheel DOMMouseScroll', function(event){
+//     console.log("HI")
+//     if (event.originalEvent.wheelDelta <= 0 && event.originalEvent.detail >= 0) {              
+//         pos=pos+50;         
+//     }
+//     else {
+//         if (pos > 1){
+//             pos=pos-50;      
+//         }
+//     }
+//     $('#superContentContainer').scrollLeft( pos)
+// });
 
-window.onload = (event) => {
+document.addEventListener("DOMContentLoaded", function(event) {
+    // - Code to execute when all DOM content is loaded. 
+    // - including fonts, images, etc.
+    // move hack tx title into view
     const titleRect = document.querySelector('#hacktxtitle').getBoundingClientRect();
     const contentContainer = document.querySelector('#superContentContainer');
-    const contentContainerRight = contentContainer.getBoundingClientRect().right
-    if (titleRect.right > contentContainerRight) {
-        contentContainer.scrollLeft = titleRect.right - contentContainerRight + 100;
+    const contentRect = contentContainer.getBoundingClientRect()
+    if (titleRect.right > contentRect.right) {
+        const contentMid = (contentRect.width) / 2.0;
+
+        const titleMid = titleRect.left + (titleRect.width / 2.0);
+        console.log(contentMid);
+        console.log(titleMid);
+        contentContainer.scrollLeft = (titleMid - contentMid);
+        
+        // titleRect.right - contentContainerRight + 100;
+        console.log(contentContainer.scrollLeft)
     }
-};
+    // if (navigator.userAgent.indexOf('Chrome') < 0 && navigator.userAgent.indexOf('Edge') < 0) {
+    //     const parallex = document.querySelector('#parallex');
+    //     parallex.style.transform = 'none';
+    //     contentContainer.style.perspective = 'none';
+    // }
+});
