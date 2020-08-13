8570c39f54dd13cdc91e519d14553fdb8fe4571a
Arjun Kunjilwar <arjunkunjilwar@utexas.edu>
2020-08-13 17:41:45 -0500

    Fix to scrolling vertically

--- secret-2020/index.js
+++ secret-2020/index.js
@@ -14,41 +14,28 @@ function imageHoverOut(element) {
     element.setAttribute('src', newSrc)
 }
 
-// var pos=0;
-// $(window).bind('mousewheel DOMMouseScroll', function(event){
-//     console.log("HI")
-//     if (event.originalEvent.wheelDelta <= 0 && event.originalEvent.detail >= 0) {              
-//         pos=pos+50;         
-//     }
-//     else {
-//         if (pos > 1){
-//             pos=pos-50;      
-//         }
-//     }
-//     $('#superContentContainer').scrollLeft( pos)
-// });
+// scroll horizontally on vertical scroll
+var contentContainer;
+function replaceVerticalScrollByHorizontal(event) {
+    if (event.deltaY != 0) {
+      // manually scroll horizonally instead
+        contentContainer.scroll(contentContainer.scrollLeft + event.deltaY , 0);
+        // prevent vertical scroll
+        event.preventDefault();
+    }
+    return;
+}
+window.addEventListener('wheel', replaceVerticalScrollByHorizontal, {passive: false});
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    // - Code to execute when all DOM content is loaded. 
-    // - including fonts, images, etc.
-    // move hack tx title into view
+    // move hack tx title into view if it is cut off
     const titleRect = document.querySelector('#hacktxtitle').getBoundingClientRect();
-    const contentContainer = document.querySelector('#superContentContainer');
+    contentContainer = document.querySelector('#superContentContainer');
     const contentRect = contentContainer.getBoundingClientRect()
     if (titleRect.right > contentRect.right) {
         const contentMid = (contentRect.width) / 2.0;
-
         const titleMid = titleRect.left + (titleRect.width / 2.0);
-        console.log(contentMid);
-        console.log(titleMid);
         contentContainer.scrollLeft = (titleMid - contentMid);
-        
-        // titleRect.right - contentContainerRight + 100;
-        console.log(contentContainer.scrollLeft)
     }
-    // if (navigator.userAgent.indexOf('Chrome') < 0 && navigator.userAgent.indexOf('Edge') < 0) {
-    //     const parallex = document.querySelector('#parallex');
-    //     parallex.style.transform = 'none';
-    //     contentContainer.style.perspective = 'none';
-    // }
+    
 });
