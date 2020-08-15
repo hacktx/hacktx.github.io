0fcd0f19b4e7a58c0595ca970ad8a359c8a91fda
Arjun Kunjilwar <arjunkunjilwar@utexas.edu>
2020-08-14 23:06:05 -0500

    Faq now collapsible; changes to assets to allow better resizing

--- secret-2020/index.js
+++ secret-2020/index.js
@@ -1,3 +1,46 @@
+var contentContainer;   // #superContentContainer
+var questionContainer;  // #question-container
+
+// scroll horizontally on vertical scroll
+function replaceVerticalScrollByHorizontal(event) {
+    if (Math.abs(event.deltaY) > 2) {
+      // manually scroll horizonally instead
+        contentContainer.scroll(contentContainer.scrollLeft + event.deltaY , 0);
+        // prevent vertical scroll
+    }
+    event.preventDefault();
+    return;
+}
+window.addEventListener('wheel', replaceVerticalScrollByHorizontal, {passive: false});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    // move hack tx title into view if it is cut off
+    const titleRect = document.querySelector('#hacktxtitle').getBoundingClientRect();
+    contentContainer = document.querySelector('#superContentContainer');
+    const contentRect = contentContainer.getBoundingClientRect();
+    if (titleRect.right > contentRect.right) {
+        const contentMid = (contentRect.width) / 2.0;
+        const titleMid = titleRect.left + (titleRect.width / 2.0);
+        contentContainer.scrollLeft = (titleMid - contentMid);
+    }
+    
+    questionContainer = document.querySelector('#questions-container');
+    const questions = document.querySelectorAll(".question");
+    for (i = 0; i < questions.length; i++) {
+        questions[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
+          if (content.style.display === "block") {
+            content.style.display = "none";
+          } else {
+            content.style.display = "block";
+          }
+          questionContainer.style.width = 'auto';
+          questionContainer.style.width = questionContainer.scrollWidth + "px";
+        });
+      }
+});
+
 function imageHover(element) {
     var currSrc = element.src; 
     const path = currSrc.indexOf('assets')
@@ -13,29 +56,3 @@ function imageHoverOut(element) {
     var newSrc = currSrc.substring(0, cutOut) + currSrc.substring(cutOut + 5);
     element.setAttribute('src', newSrc)
 }
-
-// scroll horizontally on vertical scroll
-var contentContainer;
-function replaceVerticalScrollByHorizontal(event) {
-    if (event.deltaY != 0) {
-      // manually scroll horizonally instead
-        contentContainer.scroll(contentContainer.scrollLeft + event.deltaY , 0);
-        // prevent vertical scroll
-        event.preventDefault();
-    }
-    return;
-}
-window.addEventListener('wheel', replaceVerticalScrollByHorizontal, {passive: false});
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    // move hack tx title into view if it is cut off
-    const titleRect = document.querySelector('#hacktxtitle').getBoundingClientRect();
-    contentContainer = document.querySelector('#superContentContainer');
-    const contentRect = contentContainer.getBoundingClientRect()
-    if (titleRect.right > contentRect.right) {
-        const contentMid = (contentRect.width) / 2.0;
-        const titleMid = titleRect.left + (titleRect.width / 2.0);
-        contentContainer.scrollLeft = (titleMid - contentMid);
-    }
-    
-});
